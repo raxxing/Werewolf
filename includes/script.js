e904c784c624454886d108f1abd35f3c6bfcee49
Rxx <130363444+raxxing@users.noreply.github.com>
2026-09-25 23:18:14 +0800

    commit

--- includes/script.js
+++ includes/script.js
@@ -1,46 +1,31 @@
-const buttons = document.querySelectorAll(".role-btn");
-const modals = document.querySelectorAll(".card-open");
+const modals = document.querySelectorAll(".modal");
 
-// Open card
-buttons.forEach(button => {
-    button.addEventListener("click", () => {
-        const modal = document.getElementById(button.dataset.modal);
-        modal.style.display = "flex";
-    });
-});
-
-// Close card
 modals.forEach(modal => {
 
-    const closeButton = modal.querySelector(".card-close");
-
-    closeButton.addEventListener("click", () => {
-        modal.style.display = "none";
-    });
-
-    // Close when clicking outside
-    modal.addEventListener("click", event => {
-        if (event.target === modal) {
-            modal.style.display = "none";
-        }
-    });
-
-    // Counter
     const minusBtn = modal.querySelector(".minus");
     const plusBtn = modal.querySelector(".plus");
     const numberDisplay = modal.querySelector(".number-display");
 
     let count = 0;
 
+
+    // Minus button
     minusBtn.addEventListener("click", () => {
+
         if (count > 0) {
             count--;
             numberDisplay.textContent = count;
         }
+
     });
 
+
+    // Plus button
     plusBtn.addEventListener("click", () => {
+
         count++;
         numberDisplay.textContent = count;
+
     });
+
 });
